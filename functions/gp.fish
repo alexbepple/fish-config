7506d12b0327382fbad288e34c423c3da89a90ea
Alex Bepple <alex@bepple.de>
2018-09-03 00:30:16 +0200

    $ more git aliases

---
+++ functions/gp.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 0
+function gp --description 'alias gp=g push'
+	g push $argv;
+end
---
+++ functions/st.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 0
+function st --description 'alias st=open -a sourcetree .'
+	open -a sourcetree . $argv;
+end
