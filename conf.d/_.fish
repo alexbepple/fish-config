a145d561c1882f0ff88a7fd42031198da265bb4a
Alex Bepple <alex@bepple.de>
2018-09-08 15:13:03 +0200

    . simplify

--- conf.d/_.fish
+++ conf.d/_.fish
@@ -1,3 +1,1 @@
-if status --is-interactive
-    set -g fish_user_abbreviations
-end
+set -g fish_user_abbreviations
--- config.fish
+++ config.fish
@@ -1,7 +1,5 @@
 set PATH $HOME/.cargo/bin $PATH
 
-if status --is-interactive
-    abbr --add -- - 'cd -'
-    abbr --add ... ../..
-    abbr --add c code
-end
+abbr --add -- - 'cd -'
+abbr --add ... ../..
+abbr --add c code
