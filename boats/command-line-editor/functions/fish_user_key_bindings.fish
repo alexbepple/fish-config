be2e33d8cbbfab9bab71151c603edd55cb32d86b
Alex Bepple <alex@bepple.de>
2018-09-09 21:49:00 +0200

    $ add editor key bindings

--- boats/command-line-editor/functions/fish_user_key_bindings.fish
+++ boats/command-line-editor/functions/fish_user_key_bindings.fish
@@ -1,4 +1,10 @@
 function fish_user_key_bindings
     # accept suggestions more easily, as Control-F is hard to reach
     bind ƒ forward-char
+
+    bind ª backward-word # alt-h
+    bind @ forward-word # alt-l
+
+    bind ∆ up-or-search # alt-k
+    bind º down-or-search # alt-j
 end
