3666a910d110ee3f8332b318a341cf73da7f5d9f
Alex Bepple <alex@bepple.de>
2018-09-11 01:07:43 +0200

    % test that fish loads quickly enough

--- plugins/misc/test/misc.test.fish
+++ plugins/misc/test/misc.test.fish
@@ -2,3 +2,8 @@ l > /dev/null
 test "alias 'l' works"
     $status -eq 0
 end
+
+gtime -f '%U' fish -c '' 2>&1 | cut -d . -f 2 | read -l exec_time_decimals
+test 'fish loads quickly enough'
+    $exec_time_decimals -lt 10
+end
