2446f5658cda2f6c1676fba84e2c40d3f069c40d
Alex Bepple <alex@bepple.de>
2018-09-10 04:12:11 +0200

    $ search history with fzf

---
+++ plugins/command-line-editor/functions/__if_empty_search_history_else_tab.fish
@@ -0,0 +1,20 @@
+function __set_commandline
+    if test (count $argv) -gt 0
+        commandline $argv
+    else
+        commandline ''
+    end
+end
+
+function __cle_search_history
+    history | uniq | fzf --height 40% --border --reverse --no-sort | read -l value
+    __set_commandline $value
+end
+
+function __if_empty_search_history_else_tab
+    if test -z (commandline)
+        __cle_search_history
+    else
+        eval $__cle_tab_command
+    end
+end
--- plugins/command-line-editor/functions/fish_user_key_bindings.fish
+++ plugins/command-line-editor/functions/fish_user_key_bindings.fish
@@ -7,4 +7,7 @@ function fish_user_key_bindings
 
     bind ∆ up-or-search # alt-k
     bind º down-or-search # alt-j
+
+    set -g __cle_tab_command _pisces_complete
+    bind \t __if_empty_search_history_else_tab
 end
