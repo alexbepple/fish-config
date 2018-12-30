394454de949caf0e7530f0ffe8f8904c374f6e5d
Alex Bepple <alex@bepple.de>
2018-12-30 07:29:46 +0100

    % fish_user_key_bindings → direct bind calls

---
+++ plugins/command-line-editor/conf.d/cle-key-bindings.fish
@@ -0,0 +1,10 @@
+# accept suggestions more easily, as Control-F is hard to reach
+bind ƒ forward-char
+
+bind ª backward-word # alt-h
+bind @ forward-word # alt-l
+
+bind ∆ up-or-search # alt-k
+bind º down-or-search # alt-j
+
+bind \t __if_empty_search_history_else_tab
