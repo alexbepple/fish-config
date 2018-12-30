dedc7c81e1fab66c2b7f70d68512feabb26698ac
Alex Bepple <alex@bepple.de>
2018-12-30 08:14:16 +0100

    $ use same convenience bindings in vi insert mode

--- plugins/command-line-editor/conf.d/cle-key-bindings.fish
+++ plugins/command-line-editor/conf.d/cle-key-bindings.fish
@@ -1,10 +1,20 @@
-# accept suggestions more easily, as Control-F is hard to reach
-bind ƒ forward-char
+function __cle_set_convenience_bindings
+    set bind bind -M $argv[1]
 
-bind ª backward-word # alt-h
-bind @ forward-word # alt-l
+    # accept suggestions more easily, as Control-F is hard to reach
+    $bind ƒ forward-char
 
-bind ∆ up-or-search # alt-k
-bind º down-or-search # alt-j
+    $bind ª backward-word # alt-h
+    $bind @ forward-word # alt-l
 
-bind \t __if_empty_search_history_else_tab
+    $bind ∆ up-or-search # alt-k
+    $bind º down-or-search # alt-j
+
+    $bind \t __if_empty_search_history_else_tab
+end
+
+for mode in insert default
+    __cle_set_convenience_bindings $mode
+end
+
+set -g fish_key_bindings fish_vi_key_bindings
