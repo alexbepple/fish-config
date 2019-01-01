bdc4a7ee0e3284a7d13080eeca238ca03351ddee
Alex Bepple <alex@bepple.de>
2019-01-01 20:01:55 +0100

    $ j convenience: pass args, cd to only match

--- plugins/navigate/functions/j.fish
+++ plugins/navigate/functions/j.fish
@@ -1,5 +1,5 @@
 function j
     function __extract_paths; awk '{ $1 = ""; print $0 }' | string trim; end
 
-    z -l ^/dev/null | __extract_paths | fzf | _cd_if_given
+    z -l ^/dev/null | __extract_paths | fzf -1 -q $argv | _cd_if_given
 end
