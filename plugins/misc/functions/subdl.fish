2142d741a8d250ad535140a3e07969ff994cad4c
Alex Bepple <alex@bepple.de>
2021-01-06 22:08:23 +0100

    $ set LC_ALL to a sensible default
    With LC_ALL=en_US the following would not work out of the box:
    * arc
    * beancount
    * spack
    
    My prompt issues seem to be gone with `starship`.

--- plugins/misc/functions/subdl.fish
+++ plugins/misc/functions/subdl.fish
@@ -1,5 +1,5 @@
 function subdl
     set -x OPENSUBTITLES_USERNAME alexbepple
     set -q OPENSUBTITLES_PASSWORD || echo 'Warning: OPENSUBTITLES_PASSWORD is not set.'
-    LC_ALL=C subliminal --opensubtitles "$OPENSUBTITLES_USERNAME" "$OPENSUBTITLES_PASSWORD" download $argv
+    subliminal --opensubtitles "$OPENSUBTITLES_USERNAME" "$OPENSUBTITLES_PASSWORD" download $argv
 end
